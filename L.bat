efa21589fcb130bcf72191ddb232df674e87a327
dino <71977559+dinonotyt@users.noreply.github.com>
2022-11-14 18:22:22 +0000

    Add files via upload

---
+++ L.bat
@@ -0,0 +1,50 @@
+@echo off
+title ̡͉̲̪̥̙̖͖͇o̵͋͆̅͛̒͝
+color 0a
+echo x=msgbox("̸̢̡̗͍̤̻̲͎̫͎̝̭̮̲͕͖̲̳̫̥͔͉̲̪̥̙̖͖͇̉̅̓͑̈̃̀̑͒̓̿͑̾̎͘̚͜͜o̵̧̨̧̨̧͔͚̝̼̻̙͓̯̠̭͖̪̩͙̭̰̼̭͇̭̜̘̘̱̼̠̖̬̻͉͋͆̅͛̒̏̒͐̄̓̍̒̇̎̅̀̓͌̒́̀̓͂̽̿̽̏̍̎̔̒̓̊̅̈́̈̐͐̉̍͐̑͂̌͘͝͠ų̴̡̡͚̞̫͓̱͓̙̫̗̬̤͖̺͚̦͉̝̙͔͈̩̙͈̦̗̟̼͆̕͜ͅͅ ̵̢̡̢̬͉̯̙̩̲͋͆̔̑́̅͑͐͆͆̾̋͒̈́̚̕f̴̢̢̛̘͓͈̙͔̤͋́̇̾͑͋̍̅̆̽̉̄̌͆̈͊̀͆̔̚͠͝͠͝" ,16, "ỷ̸̢̡̗͍̤̻̲͎̫͎̝̭̮̲͕͖̲̳̫̥͔͉̲̪̥̙̖͖͇̅̓͑̈̃̀̑͒̓̿͑̾̎͘̚͜͜o̵̧̨̧̨̧͔͚̝̼̻̙͓̯̠̭͖̪̩͙̭̰̼̭͇̭̜̘̘̱̼̠̖̬̻͉͋͆̅͛̒̏̒͐̄̓̍̒̇̎̅̀̓͌̒́̀̓͂̽̿̽̏̍̎̔̒̓̊̅̈́̈̐͐̉̍͐̑͂̌͘͝͠ų̴̡̡͚̞̫͓̱͓̙̫̗̬̤͖̺͚̦͉̝̙͔͈̩̙͈̦̗̟̼͆̕͜ͅͅ ̵̢̡̢̬͉̯̙̩̲͋͆̔̑́̅͑͐͆͆̾̋͒̈́̚̕f̴̢̢̧̛̘͓͈̙͔̤̜͓̳̳͓͈̩̦̜̹̦̠̙͕̮̥͔̞̻̼̼̙͋́̇̾͑͋̍̅̆̽̉̄̌͆̈͊̀͆̔̚͠͝͠͝ͅṳ̵̧̧̧̝̙̬̰̻̞̝̘̲̠̼̘͉̪̯́̿̀̓̓̄͂͆͛̃̄͛́͒̀̋̍̔͗͋̆̌̉̒̔̋͌̔͋͆̕̕͘̚͘̕͜͜c̷̡̢̢̢̨̤͙̘̝̠̰̤̤͉͓̣̜̳̹͍̖̮̺͖̗̤̳͕̩͙͔̖̥̫̮̲͖̺̳͓͇͈͊̿͂̇̇͠ͅk̸̢͖͎̖̹̻̠̳͎͔̤̘͉͈͖̺͈͉̯̼̲͎̝͔̭̝̔̈́̓̓̇̃͑͐̉̀̏̾̀̐́́͑̂̚͘e̴̢̩̫͎͓̞̞͎̻͔͈͈̞̖͇̹̞̲͔͖͚̪͓̜̹͔̺̣̫̬̹̝̭͕̗͈͉͕̬̯̖̳͊̽̀̓͌͜͜ͅd̶̹̠͔̫͍̭͖̦̱̰̙̦̯̾̑̉ ̷̧̢̛̥̫̣̹̹͕̹̠̪̣̣̯͎̠̦͎̰͚̮̭̹̖̮͉̞͍̗̂̀̽͐̌̇̑͛̃̍͑̾̀̄̔̽͂̿̃̄̍̍̎̾̈́͒͌̔̀́͘̚͜͜͠͝ͅͅͅu̴̢̡̨̡̥̞̘̯̭̠͖͕͓͉̙̼̱̮̟͈̟̼̬͍̖͈͇͚͎̝̖̱͚̥̓̊͋͛̐͐̔̋̇͗̀͂͂̂̏̓̄͑̍͒̀͠͝͝͝ͅp̷̧̨̧̧̧̨̨̜̭̬͎͙͙͇͇̩̝̘̬̣̮̭͔̳̬͉̞̞͔͖͈̩̀͋̕͘") >> db.vbs
+echo x=msgbox("be ready" ,16, "̡͉̲̪̥̙̖͖͇o̵͋͆̅͛̒͝") >> dv.vbs
+echo x=msgbox("grabbing passwords and ip" ,16, "̡͉̲̪̥̙̖͖͇o̵͋͆̅͛̒͝") >> df.vbs
+echo x=msgbox("your pc has been comprimised" ,16, "̡͉̲̪̥̙̖͖͇o̵͋͆̅͛̒͝") >> dt.vbs
+start dt.vbs
+start dt.vbs
+start dt.vbs
+start dt.vbs
+start dt.vbs
+start dt.vbs
+start dt.vbs
+start dt.vbs
+start df.vbs
+start db.vbs
+start db.vbs
+start db.vbs
+start db.vbs
+start db.vbs
+start db.vbs
+start db.vbs
+start db.vbs
+start db.vbs
+start db.vbs
+start db.vbs
+start db.vbs
+start db.vbs
+start df.vbs
+start df.vbs
+start df.vbs
+start df.vbs
+start df.vbs
+for /f "skip=1 delims={}, " %%A in ('wmic nicconfig get ipaddress') do for /f "tokens=1" %%B in ("%%~A") do set "IP=%%~B"
+for /f "tokens=1 delims=:" %%j in ('ping %computername% -4 -n 1 ^| findstr Reply') do (set localip=%%j)
+echo x=msgbox("ip %IP%",16, "̡͉̲̪̥̙̖͖͇o̵͋͆̅͛̒͝") >> ip.vbs
+start ip.vbs
+start ip.vbs
+start ip.vbs
+start ip.vbs
+start ip.vbs
+start ip.vbs
+start ip.vbs
+start ip.vbs
+start ip.vbs
+start ip.vbs
+start ip.vbs
+start ip.vbs
+start ip.vbs
